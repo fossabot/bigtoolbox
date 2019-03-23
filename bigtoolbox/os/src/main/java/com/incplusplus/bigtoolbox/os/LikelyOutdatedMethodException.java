aeaeab7127165b73341c472211f0982b4b94178f
IncPlusPlus <cloherty.ryan@gmail.com>
2019-03-23 09:03:53 -0400

    Quick correction in LikelyOutdatedMethodException

--- bigtoolbox/os/src/main/java/com/incplusplus/bigtoolbox/os/LikelyOutdatedMethodException.java
+++ bigtoolbox/os/src/main/java/com/incplusplus/bigtoolbox/os/LikelyOutdatedMethodException.java
@@ -4,7 +4,7 @@ public class LikelyOutdatedMethodException extends RuntimeException
 {
 	private static String cause = "A method that was called is in an unexpected state. \n" +
 			"This is likely because the environment the method expects is outdated. \n" +
-			"Further details: ";
+			"Thrown from: ";
 
 	/**
 	 * A method that depends on very specific strings given from the OS may
