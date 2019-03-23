47184b1fd372c843d2304dd805eeeb95dd9e573f
IncPlusPlus <cloherty.ryan@gmail.com>
2019-03-23 09:07:35 -0400

    Added details to IncorrectOperatingSystemException

--- bigtoolbox/os/src/main/java/com/incplusplus/bigtoolbox/os/IncorrectOperatingSystemException.java
+++ bigtoolbox/os/src/main/java/com/incplusplus/bigtoolbox/os/IncorrectOperatingSystemException.java
@@ -2,8 +2,12 @@ package com.incplusplus.bigtoolbox.os;
 
 public class IncorrectOperatingSystemException extends RuntimeException
 {
+	/**
+	 * This is thrown when a specific OS from the opsys package is instantiated and
+	 * is not actually the OS of the host that is running this program.
+	 */
 	public IncorrectOperatingSystemException()
 	{
-		super();
+		super("An opsys class was instantiated but does not match the host OS.");
 	}
 }
