03f3dd95e17a6c0d0e0c604f01326a0ef1ce941c
Ryan Cloherty <cloherty.ryan@gmail.com>
2019-03-31 19:00:16 -0400

    Improved ease of use of LikelyOutdatedMethodException

--- bigtoolbox/os/src/main/java/com/incplusplus/bigtoolbox/os/LikelyOutdatedMethodException.java
+++ bigtoolbox/os/src/main/java/com/incplusplus/bigtoolbox/os/LikelyOutdatedMethodException.java
@@ -12,12 +12,10 @@ public class LikelyOutdatedMethodException extends RuntimeException
 	 * not know how to parse that string properly.
 	 * This can and likely will be caused by the OSFamily receiving an update that
 	 * changes the expected string format.
-	 * @param culpritPackage The package this was thrown from
-	 * @param culpritClass The class this was thrown from
-	 * @param culpritMethod The method this was thrown from
+	 * @param throwingClass The class that threw this exception. Best practice is to send in getClass()
 	 */
-	public LikelyOutdatedMethodException(String culpritPackage, String culpritClass, String culpritMethod)
+	public LikelyOutdatedMethodException(Class<?> throwingClass)
 	{
-		super(String.format("%s %s in class %s in package %s", cause, culpritMethod, culpritClass, culpritPackage));
+		super(String.format("%s %s in class %s in package %s", cause, throwingClass.getEnclosingMethod().getName(), throwingClass.getName(), throwingClass.getPackage().toString()));
 	}
 }
